0756f86158a8ba8f313985e15d9bd9985434edc8
tjtharrison <gh@tjth.co>
2022-06-24 21:31:42 +0100

    PR/Separated SSE [minor]

--- bucket.tf
+++ bucket.tf
@@ -1,15 +1,5 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
-    }
-  }
-  versioning {
-    enabled = var.enable_versioning
-  }
 }
 
 resource "aws_s3_bucket_versioning" "this" {
--- bucket_security.tf
+++ bucket_security.tf
@@ -11,3 +11,13 @@ resource "aws_s3_bucket_public_access_block" "this" {
   restrict_public_buckets = var.restrict_public_buckets
   ignore_public_acls      = var.ignore_public_acls
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket =  aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
